c4f237d1ae84d89035116e4f8350c5e155cb2ca0
famgz <famgz@proton.me>
2024-06-05 15:14:45 -0300

    fix: make SearchBox width flexible

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -2,7 +2,7 @@ import { CiSearch } from 'react-icons/ci';
 
 export default function SearchBox() {
   return (
-    <div className="inline-flex h-[50px] w-[372px] items-center justify-start gap-4 rounded-lg border p-4 text-light hover:border-primary">
+    <div className="inline-flex h-[50px] w-[min(372px,100%)] items-center justify-start gap-4 rounded-lg border p-4 text-light hover:border-primary">
       <CiSearch size={16} className="font-bold text-muted" />
       <span>Pesquisar</span>
     </div>
