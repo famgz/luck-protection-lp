7966b77d6a390154943c148d1402b5d18b44b55e
famgz <famgz@proton.me>
2024-06-06 14:10:48 -0300

    fix: ensure scroll will be reseted on navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from 'src/components/Footer';
 import Header from 'src/components/Header';
+import ScrollToTop from 'src/components/ScrollToTop';
 import HomePage from 'src/pages/Home';
 import NotFound from 'src/pages/NotFound';
-import SingleProductPage from 'src/pages/SingleProduct';
 import ProductsPage from 'src/pages/Products';
+import SingleProductPage from 'src/pages/SingleProduct';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
 function WrappedApp() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <div className="flex min-h-screen flex-col">
         <Header />
         <main className="flex-1">
